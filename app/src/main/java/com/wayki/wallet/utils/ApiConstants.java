21265b59f9785380815358a92b6d062c3a489987
Administrator <xinshuhao@sina.cn>
2018-11-29 10:34:00 +0800

    修复一些问题

--- app/src/main/java/com/wayki/wallet/utils/ApiConstants.java
+++ app/src/main/java/com/wayki/wallet/utils/ApiConstants.java
@@ -4,30 +4,30 @@ import com.wayki.wallet.BuildConfig;
 
 public class ApiConstants {
     public  static final String baseurl= BuildConfig.API_HOST;
-    public static final String upgrade= baseurl+"api/app/upgrade";
-    public static final String bindwallet=baseurl +"api/customer/bind/wallet";
-    public static final String bindinfo=baseurl +"api/customer/ref/wallet/info";
-    public static final String getaccountinfo=baseurl +"api/customer/account/getinfo";
-    public  static final String getrate=baseurl+"api/sys/exchange/rate";
-    public  static final String transaction_wicc=baseurl+"api/exchange/trans/wicc";
-    public static final String  blocknumber=baseurl+"api/chain/height";
-    public static final String  isactive=baseurl+"api/address/isActive";
-    public static final String  wicc2token=baseurl+"api/exchange/trans/wicc/token";
-    public static final String  token2wicc2=baseurl+"api/exchange/trans/token/wicc";
-    public static final String  exchanglist=baseurl+"api/exchange/exchangeOrders";
-    public static final String  tradelist=baseurl+"api/customer/account/log/logs";
-    public static final String  getcontract=baseurl+"api/exchange/contract";
-    public static final String  getwicc=baseurl+"api/sys/link/100";
+    public static final String upgrade= baseurl+"";
+    public static final String bindwallet=baseurl +"";
+    public static final String bindinfo=baseurl +"";
+    public static final String getaccountinfo=baseurl +"";
+    public  static final String getrate=baseurl+"";
+    public  static final String transaction_wicc=baseurl+"";
+    public static final String  blocknumber=baseurl+"";
+    public static final String  isactive=baseurl+"";
+    public static final String  wicc2token=baseurl+"";
+    public static final String  token2wicc2=baseurl+"";
+    public static final String  exchanglist=baseurl+"";
+    public static final String  tradelist=baseurl+"";
+    public static final String  getcontract=baseurl+"";
+    public static final String  getwicc=baseurl+"";
 
-    public static final String  tradedetail=baseurl+"api/customer/account/log/detail/";
+    public static final String  tradedetail=baseurl+"";
 
-    public static final String  bottom_item=baseurl+"api/sys/tab/menu/list";
+    public static final String  bottom_item=baseurl+"";
 
-    public static final String  reward=baseurl+"api/walletaddress/sys/reward/list";
+    public static final String  reward=baseurl+"";
 
-    public  static final String wtestrate=baseurl+"api/sys/exchange/WTEST/rate";
+    public  static final String wtestrate=baseurl+"";
 
 
-    public  static final String web_login= baseurl+"login";
+    public  static final String web_login= baseurl+"";
 
 }
